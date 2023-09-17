90dd18b0798ff921cb90da383f5678588f68216b
Barun <tiwarivarun13@gmail.com>
2023-09-17 17:59:09 +0530

    learning props

---
+++ src/components/TextForms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function textForms() {
+  return (
+    <div>
+      
+<div class="mb-3">
+  <label for="myBox" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="myBox" rows="3"></textarea>
+</div>
+    </div>
+  )
+}
